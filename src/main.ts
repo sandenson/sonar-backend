1abd2b9d71bc142b056f80011136db62dfe3a97b
sandenson <lgabrielsandes@gmail.com>
2026-06-21 19:17:08 -0300

    feat: Set up Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sonar')
+    .setDescription('Sonar - suas músicas, suas histórias')
+    .setVersion('1.0')
+    .build();
+
+  SwaggerModule.setup('api', app, () =>
+    SwaggerModule.createDocument(app, config),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
