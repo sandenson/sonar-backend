5cb7381eae7a1daaca4c6dec120a61f3fd9854ac
sandenson <lgabrielsandes@gmail.com>
2026-06-24 14:01:53 -0300

    fix: "Create users table" migration: fix "down" method; make username and email indexes partial to exclude soft deleted rows.

--- db/migrations/1782153714205-create-users-table.ts
+++ db/migrations/1782153714205-create-users-table.ts
@@ -7,17 +7,17 @@ export class CreateUsersTable1782153714205 implements MigrationInterface {
     await queryRunner.query(
       `CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying(50) NOT NULL, "email" character varying(200) NOT NULL, "password" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"));
       
-      CREATE UNIQUE INDEX "USER_UNIQUE_USERNAME_LOWER" ON "users" (LOWER("username"));
+      CREATE UNIQUE INDEX "USER_UNIQUE_USERNAME_LOWER" ON "users" (LOWER("username")) WHERE deleted_at IS NULL;
       
-      CREATE UNIQUE INDEX "USER_UNIQUE_EMAIL_LOWER" ON "users" (LOWER("email"));`,
+      CREATE UNIQUE INDEX "USER_UNIQUE_EMAIL_LOWER" ON "users" (LOWER("email")) WHERE deleted_at IS NULL;`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      CREATE UNIQUE INDEX "USER_UNIQUE_EMAIL_LOWER" ON "users" (LOWER("email"));
+      DROP INDEX CONCURRENTLY IF EXISTS "USER_UNIQUE_EMAIL_LOWER";
       
-      CREATE UNIQUE INDEX "USER_UNIQUE_USERNAME_LOWER" ON "users" (LOWER("username"));
+      DROP INDEX CONCURRENTLY IF EXISTS "USER_UNIQUE_USERNAME_LOWER";
       
       DROP TABLE "users";`);
   }
