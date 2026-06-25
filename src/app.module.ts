824250c80c4e622828180b5aa409d8f9163076a7
sandenson <lgabrielsandes@gmail.com>
2026-06-25 19:27:30 -0300

    fix: db/datasource importing path.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSourceOptions } from 'db/datasource';
 import * as Joi from 'joi';
+import { dataSourceOptions } from '../db/datasource';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
